859f758b345530ac7c5247dd62a5952bafdf0d7b
Sebastian Mauer <mauimauer@gmail.com>
2014-02-27 04:09:53 +0100

    Fix failing TravisCI Build

--- karma.conf.js
+++ karma.conf.js
@@ -12,6 +12,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'http://maps.googleapis.com/maps/api/js?sensor=false',
+      'app/bower_components/jquery/dist/jquery.js',
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
       'app/bower_components/angular-resource/angular-resource.js',
