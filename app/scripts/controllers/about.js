5237291e0465755c2479b59449137d7561dfe2ea
Joshua Woodward <jw@woodwardmedia.net>
2014-05-12 20:11:26 -0700

    Init

--- app/scripts/controllers/about.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('gdgxHubApp')
-	.controller('AboutCtrl', function ($scope, $http) {
-		$http.get("https://api.github.com/repos/gdg-x/hub/contributors").success(function(data, status, headers, config) {
-			$scope.contributors = data;
-		});
-	});
--- app/scripts/controllers/developer.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('gdgxHubApp')
-  .controller('ApiCtrl', function ($scope, $http) {
-  	$http.get('/api/v1/rest').success(function(data) {
-  		$scope.restDiscovery = data;
-  	});
-  });
