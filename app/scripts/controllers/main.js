6ee589aa431c1932597c1b4b15489b57df0ed4f1
Sebastian Mauer <mauimauer@gmail.com>
2014-05-14 23:58:57 +0200

    Remove bogus call to /api/awesomeThings

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,7 +2,4 @@
 
 angular.module('gdgxHubApp')
   .controller('MainCtrl', function ($scope, $http) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
   });
