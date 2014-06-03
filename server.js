7a2eccfea6602590639e7e2a2d7293a5b6518325
Sebastian Mauer <mauimauer@gmail.com>
2014-06-04 00:00:17 +0200

    Back to the old-school crash handling.

--- server.js
+++ server.js
@@ -5,130 +5,103 @@ var express = require('express'),
     fs = require('fs'),
     cluster = require('cluster'),
     http = require('http'),
-    nodemailer = require('nodemailer'),
     numCPUs = require('os').cpus().length,
     mongoose = require('mongoose');
 /**
  * Main application file
  */
 console.log2 = console.log;
-console.log = function(){
-	var args = Array.prototype.slice.call(arguments);
+console.log = function() {
+    var args = Array.prototype.slice.call(arguments);
 
-	if(args.length > 1) {
-		args[0] = "["+process.pid+"] "+args[0];
-		this.log2.apply(this, args);
-	} else {
-		this.log2("["+process.pid+"] "+args[0]);
-	}
+    if (args.length > 1) {
+        args[0] = "[" + process.pid + "] " + args[0];
+        this.log2.apply(this, args);
+    } else {
+        this.log2("[" + process.pid + "] " + args[0]);
+    }
 };
 
 if (cluster.isMaster) {
 
-  // Fork workers.
-  for (var i = 0; i < numCPUs; i++) {
-  	setTimeout(function() {
-	    var worker = cluster.fork();
-	    console.log('worker started, PID '+ worker.process.pid);
-  	}, (i+1)*5000); 
-  }
+    // Fork workers.
+    for (var i = 0; i < numCPUs; i++) {
+        setTimeout(function() {
+            var worker = cluster.fork();
+            console.log('worker started, PID ' + worker.process.pid);
+        }, (i + 1) * 5000);
+    }
 
-  cluster.on('disconnect', function(deadWorker) {
-    // Restart the worker
-    var worker = cluster.fork();
+    cluster.on('exit', function(deadWorker, code, signal) {
+        // Restart the worker
+        var worker = cluster.fork();
 
-    // Note the process IDs
-    var newPID = worker.process.pid;
-    var oldPID = deadWorker.process.pid;
+        // Note the process IDs
+        var newPID = worker.process.pid;
+        var oldPID = deadWorker.process.pid;
 
-    // Log the event
-    console.log('worker '+oldPID+' died.');
-    console.log('worker '+newPID+' born.');
-  });
+        // Log the event
+        console.log('worker ' + oldPID + ' died. Code: ' + code + ", Signal: " + signal);
+        console.log('worker ' + newPID + ' born.');
+    });
 
 } else {
-	// Default node environment to development
-	process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+    // Default node environment to development
+    process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-	// Application Config
-	var config = require('./lib/config/config');
+    // Application Config
+    var config = require('./lib/config/config');
 
-	process.on('uncaughtException', function(err) {
-	  if(config.mail && config.mail.error_recipient) {
-	  	console.log("Mailing error report.");
-	  	var transport = nodemailer.createTransport(config.mail.transport, {});
-	  	transport.sendMail({
-	  		from: config.mail.sender,
-	  		to: config.mail.error_recipient,
-	  		subject: "Worker "+ process.pid + " crashed",
-	  		generateTextFromHTML: true,
-	  		html: "One of the workers of the Hub just crashed. Please check this Stacktrace and take action if necessary:<br/><br/>" + JSON.stringify(err.stack).split("\\n").join("<br />")
-	  	})
-	  }
+    // Disable NewRelic for now.
+    //var newrelic = require('newrelic');
 
-	  try {
-        // Ten minutes to let other connections finish:
-        var killTimer = setTimeout(function() {
-          process.exit(1);
-        }, 60000);
-        killTimer.unref(); // Don't stay up just for the timer
-        cluster.worker.disconnect(); // Stop taking new requests
-      } catch (err2) {
-        console.log("Error handling error!: " + err2);
-      }
-	});
+    // Connect to database
+    var db = mongoose.connect(config.mongo.uri, config.mongo.options);
 
+    // Bootstrap models
+    var modelsPath = path.join(__dirname, 'lib/models');
+    fs.readdirSync(modelsPath).forEach(function(file) {
+        console.log("Loading model..." + file.replace(".js", ""));
+        require(modelsPath + '/' + file);
+    });
 
-	// Disable NewRelic for now.
-	//var newrelic = require('newrelic');
+    require('./lib/fixtures')();
 
-	// Connect to database
-	var db = mongoose.connect(config.mongo.uri, config.mongo.options);
+    var risky = require('./lib/risky');
 
-	// Bootstrap models
-	var modelsPath = path.join(__dirname, 'lib/models');
-	fs.readdirSync(modelsPath).forEach(function (file) {
-	  console.log("Loading model..."+ file.replace(".js",""));
-	  require(modelsPath + '/' + file);
-	});
+    // Passport Configuration
+    require('./lib/config/passport')();
 
-	require('./lib/fixtures')();
+    if (config.env == 'production') {
+        var myId = process.env.OPENSHIFT_GEAR_UUID + "";
+        if (cluster.isWorker) {
+            myId = process.env.OPENSHIFT_GEAR_UUID + "-" + cluster.worker.process.pid;
+        }
+        risky.connect({
+            port: config.redis.port,
+            host: config.redis.host,
+            auth: config.redis.password,
+            id: myId,
+            scope: 'risky'
+        });
+    }
+    require('./lib/tasks')();
 
-	var risky = require('./lib/risky');
+    var app = express();
 
-	// Passport Configuration
-	require('./lib/config/passport')();
+    // Express settings
+    require('./lib/config/express')(app);
 
-	if(config.env == 'production') {
-		var myId = process.env.OPENSHIFT_GEAR_UUID+"";
-		if(cluster.isWorker) {
-			myId = process.env.OPENSHIFT_GEAR_UUID + "-" + cluster.worker.process.pid;
-		}
-		risky.connect({
-			port: config.redis.port,
-			host: config.redis.host,
-			auth: config.redis.password,
-			id: myId,
-			scope: 'risky'
-		});
-	}
-	require('./lib/tasks')();
+    // Routing
+    require('./lib/routes')(app);
 
-	var app = express();
+    require('./lib/cron')();
 
-	// Express settings
-	require('./lib/config/express')(app);
+    // Start server
+    app.listen(config.port, config.hostname, function() {
+        console.log('Express server listening on port %d in %s mode', config.port, app.get('env'));
+    });
 
-	// Routing
-	require('./lib/routes')(app);
-
-	require('./lib/cron')();
-
-	// Start server
-	app.listen(config.port, config.hostname, function () {
-	  console.log('Express server listening on port %d in %s mode', config.port, app.get('env'));
-	});
-
-	// Expose app
-	exports = module.exports = app;
+    // Expose app
+    exports = module.exports = app;
 }
