bbaed009724376ceaa3e13b9bf1897985671ccc3
Michael Prentice <splaktar@gmail.com>
2017-03-17 17:03:24 -0400

    update(server): remove Redis, clustering. Convert session storage to Mongo.
    upgrade to latest Express and related libraries
    convert tasks to be triggered directly instead of via Redis+Clustering
    convert cron to call new tasks
    remove deprecated express-annotations
    remove API docs since they depended upon express-annotations
    convert rate metering to in-memory only since Redis is no longer an option
    temporarily disable CSRF protection
    
    Fixes #96 #80

--- app/developer/developer.controller.js
+++ app/developer/developer.controller.js
@@ -2,10 +2,6 @@
 
 angular.module('gdgxHubApp').controller('DeveloperCtrl', DeveloperCtrl);
 
-DeveloperCtrl.$inject = ['$scope', '$http'];
+DeveloperCtrl.$inject = [];
 
-function DeveloperCtrl($scope, $http) {
-  $http.get('/api/v1/rest').success(function(data) {
-    $scope.restDiscovery = data;
-  });
-}
+function DeveloperCtrl() {}
