2d03d52ccd350498de500f4e68f171afbb5dc457
Michael Prentice <splaktar@gmail.com>
2017-03-17 11:53:09 -0400

    remove(gplus): remove G+ metric tracking per chapter (#111)
    * remove(gplus): remove G+ metric tracking per chapter
    
    Meetup.com will be the new source of metrics
    This means removing the chapter metrics page in the UI (only displayed chapter circle count and +1s).
    Removed old `/events/devfest` route that displayed info from DevFest Season 2014.
    Also removed non-functional routes in UI for `/events/tags` and `/events/tags/:tag`.
    Remove daily metrics as they aren't used and are mostly just G+ data.
    Remove old cruft: new relic, ng-scenario stuff
    Improve error logging for Devsite APIs
    
    Closes #103
    
    * fix(devsite): only log error if one exists

--- app/scripts/hub.module.js
+++ app/scripts/hub.module.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('gdgxHubApp', ['ngCookies', 'ngResource', 'ngSanitize', 'ngRoute', 'd3', 'moment-timezone',
-  'googlechart', 'ui.calendar', 'ui.bootstrap', 'uiGmapgoogle-maps', 'gdgxHubApp.directives.gplus',
-  'gdgxHubApp.directives.metrics', 'gdgxHubApp.directives.d3', 'gdgxHubApp.directives.moment', 'directive.g+signin'
+  'googlechart', 'ui.calendar', 'ui.bootstrap', 'uiGmapgoogle-maps', 'gdgxHubApp.directives.gplus', 'gdgxHubApp.directives.d3',
+  'gdgxHubApp.directives.moment', 'directive.g+signin'
 ])
   .run(function ($rootScope, $location, $http, $window, $log, Utilities, config) {
     $rootScope.user = {
