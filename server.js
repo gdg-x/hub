0c7d770240e27c1dfa7e38d8dc30817d5150b965
Michael Prentice <splaktar@gmail.com>
2017-03-17 17:03:24 -0400

    update(server): remove Redis, clustering. Convert session storage to Mongo.
    upgrade to latest Express and related libraries
    convert tasks to be triggered directly instead of via Redis+Clustering
    convert cron to call new tasks
    remove deprecated express-annotations
    remove API docs since they depended upon express-annotations
    convert rate metering to in-memory only since Redis is no longer an option
    temporarily disable CSRF protection
    
    Fixes #96 #80

--- server.js
+++ server.js
@@ -12,8 +12,6 @@ if (env && !env.error) {
 var express = require('express'),
   path = require('path'),
   fs = require('fs'),
-  cluster = require('cluster'),
-  numCPUs = require('os').cpus().length,
   mongoose = require('mongoose');
 
 /**
@@ -31,106 +29,46 @@ console.log = function () {
   }
 };
 
-function initWorker(worker) {
-  var listeners;
+// Default node environment to development
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-  listeners = worker.process.listeners('exit')[0];
-  var exit = listeners[Object.keys(listeners)[0]];
+// Application Config
+var config = require('./lib/config/config');
 
-  listeners = worker.process.listeners('disconnect')[0];
-  var disconnect = listeners[Object.keys(listeners)[0]];
+// Connect to database
+var db = mongoose.connect(config.mongo.uri, config.mongo.options); // jshint ignore:line
 
-  worker.process.removeListener('exit', exit);
-  worker.process.once('exit', function(exitCode, signalCode) {
-    if (worker.state !== 'disconnected') {
-      disconnect();
-    }
-    exit(exitCode, signalCode);
-  });
-}
+// Bootstrap models
+var modelsPath = path.join(__dirname, 'lib/models');
+fs.readdirSync(modelsPath).forEach(function (file) {
+  console.log('Loading model...' + file.replace('.js', ''));
+  require(modelsPath + '/' + file);
+});
 
-if (cluster.isMaster) {
-  var i;
-  // Fork workers.
-  for (i = 0; i < numCPUs; i++) {
-    setTimeout(function () {
-      var worker = cluster.fork();
-      initWorker(worker);
-      console.log('Worker started, PID ' + worker.process.pid);
-    }, (i + 1) * 5000); // jshint ignore:line
-  }
+// Import static data
+require('./lib/fixtures')();
 
-  cluster.on('exit', function (deadWorker, code, signal) {
-    // Restart the worker
-    var worker = cluster.fork();
-    initWorker(worker);
+// Passport Configuration
+require('./lib/config/passport')();
 
-    // Note the process IDs
-    var newPID = worker.process.pid;
-    var oldPID = deadWorker.process.pid;
+// Initialize admin task handlers
+require('./lib/tasks')();
 
-    // Log the event
-    console.log('worker ' + oldPID + ' died. Code: ' + code + ', Signal: ' + signal);
-    console.log('worker ' + newPID + ' born.');
-  });
-} else {
-  // Default node environment to development
-  process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+var app = express();
 
-  // Application Config
-  var config = require('./lib/config/config');
+// Express settings
+require('./lib/config/express')(app);
 
-  // Connect to database
-  var db = mongoose.connect(config.mongo.uri, config.mongo.options); // jshint ignore:line
+// Routing
+require('./lib/routes')(app);
 
-  // Bootstrap models
-  var modelsPath = path.join(__dirname, 'lib/models');
-  fs.readdirSync(modelsPath).forEach(function (file) {
-    console.log('Loading model...' + file.replace('.js', ''));
-    require(modelsPath + '/' + file);
-  });
+// Initialize cron jobs
+require('./lib/cron')();
 
-  // Import static data
-  require('./lib/fixtures')();
+// Start server
+app.listen(config.port, config.hostname, function () {
+  console.log('Express server listening on port %d in %s mode', config.port, app.get('env'));
+});
 
-  var risky = require('./lib/risky');
-
-  // Passport Configuration
-  require('./lib/config/passport')();
-
-  if (config.env === 'production' && config.redis) {
-    var myId = 'workerId';
-    if (cluster.isWorker) {
-      myId = 'worker-' + cluster.worker.process.pid;
-    }
-    risky.connect({
-      port: config.redis.port,
-      host: config.redis.host,
-      auth: config.redis.password,
-      id: myId,
-      scope: 'risky'
-    });
-  }
-
-  // Initialize admin task handlers
-  require('./lib/tasks')();
-
-  var app = express();
-
-  // Express settings
-  require('./lib/config/express')(app);
-
-  // Routing
-  require('./lib/routes')(app);
-
-  // Initialize cron jobs
-  require('./lib/cron')();
-
-  // Start server
-  app.listen(config.port, config.hostname, function () {
-    console.log('Express server listening on port %d in %s mode', config.port, app.get('env'));
-  });
-
-  // Expose app
-  exports = module.exports = app;
-}
+// Expose app
+exports = module.exports = app;
