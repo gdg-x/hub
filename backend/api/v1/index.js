ed9ffd67bd20dae301ab55dc34f01501ff23ac9e
Vikram Tiwari <vikramtheone1@gmail.com>
2017-03-18 17:42:52 -0700

    remove env

--- backend/api/v1/index.js
+++ backend/api/v1/index.js
@@ -3,4 +3,5 @@
 const router = require('express').Router()
 router.use('/health', require('./health'))
 router.use('/user', require('./user'))
+router.use('/admin', require('./admin'))
 module.exports = router
